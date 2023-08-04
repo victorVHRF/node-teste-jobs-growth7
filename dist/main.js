7edc7328b9dba923d603858f92688756a33525ad
Victor Hugo Rosa Fosneca <victor.vhrf@gmail.com>
2023-08-03 22:47:35 -0300

    chore: remocao dos arquivos do diretorio dist

--- dist/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jobScheduler_js_1 = require("./jobScheduler.js");
-const mockJobs_js_1 = require("./mock/mockJobs.js");
-const scheduledJobs = (0, jobScheduler_js_1.scheduleJobs)(mockJobs_js_1.mockJobs);
-console.log(scheduledJobs);
